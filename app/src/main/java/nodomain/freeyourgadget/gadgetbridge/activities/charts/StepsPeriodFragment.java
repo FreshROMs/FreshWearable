Vanko1985/New

Add option to hide sleep and steps balance (#2476)

app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/charts/StepsPeriodFragment.java
@@ -52,6 +52,7 @@ public class StepsPeriodFragment extends StepsFragment<StepsPeriodFragment.Steps
     protected int CHART_TEXT_COLOR;
     protected int TEXT_COLOR;
     protected int STEPS_GOAL;
+    protected boolean SHOW_BALANCE;
 
     protected int BACKGROUND_COLOR;
     protected int DESCRIPTION_COLOR;
@@ -89,6 +90,13 @@ public class StepsPeriodFragment extends StepsFragment<StepsPeriodFragment.Steps
         STEPS_GOAL = GBApplication.getPrefs().getInt(ActivityUser.PREF_USER_STEPS_GOAL, ActivityUser.defaultUserStepsGoal);
 
         mBalanceView = rootView.findViewById(R.id.balance);
+
+        SHOW_BALANCE = GBApplication.getPrefs().getBoolean("charts_show_balance_steps", true);
+        if (SHOW_BALANCE) {
+            mBalanceView.setVisibility(View.VISIBLE);
+        } else {
+            mBalanceView.setVisibility(View.GONE);
+        }
 
         setupStepsChart();
         refresh();

app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/charts/WeekSleepChartFragment.java
@@ -69,6 +69,8 @@ public class WeekSleepChartFragment extends AbstractWeekChartFragment {
 
     private TextView mBalanceView;
 
+    protected boolean SHOW_BALANCE;
+
     public static WeekSleepChartFragment newInstance ( int totalDays ) {
         WeekSleepChartFragment fragmentFirst = new WeekSleepChartFragment();
         Bundle args = new Bundle();
@@ -144,6 +146,13 @@ public class WeekSleepChartFragment extends AbstractWeekChartFragment {
         sleepDatesText = rootView.findViewById(R.id.sleep_dates);
 
         mBalanceView = rootView.findViewById(R.id.balance);
+
+        SHOW_BALANCE = GBApplication.getPrefs().getBoolean("charts_show_balance_sleep", true);
+        if (SHOW_BALANCE) {
+            mBalanceView.setVisibility(View.VISIBLE);
+        } else {
+            mBalanceView.setVisibility(View.GONE);
+        }
 
         if (!supportsSleepScore()) {
             sleepScoreWrapper.setVisibility(View.GONE);

FreshROMs/FreshWearable

Signed-off-by: John Vincent Corcega <git@tenseventyseven.xyz>

app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/charts/StepsPeriodFragment.java
@@ -52,6 +52,7 @@ public class StepsPeriodFragment extends StepsFragment<StepsPeriodFragment.Steps
     protected int CHART_TEXT_COLOR;
     protected int TEXT_COLOR;
     protected int STEPS_GOAL;
+    protected boolean SHOW_BALANCE;
 
     protected int BACKGROUND_COLOR;
     protected int DESCRIPTION_COLOR;
@@ -89,6 +90,13 @@ public class StepsPeriodFragment extends StepsFragment<StepsPeriodFragment.Steps
         STEPS_GOAL = WearableApplication.getPrefs().getInt(ActivityUser.PREF_USER_STEPS_GOAL, ActivityUser.defaultUserStepsGoal);
 
         mBalanceView = rootView.findViewById(R.id.balance);
+
+        SHOW_BALANCE = WearableApplication.getPrefs().getBoolean("charts_show_balance_steps", true);
+        if (SHOW_BALANCE) {
+            mBalanceView.setVisibility(View.VISIBLE);
+        } else {
+            mBalanceView.setVisibility(View.GONE);
+        }
 
         setupStepsChart();
         refresh();

app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/charts/WeekSleepChartFragment.java
@@ -69,6 +69,8 @@ public class WeekSleepChartFragment extends AbstractWeekChartFragment {
 
     private TextView mBalanceView;
 
+    protected boolean SHOW_BALANCE;
+
     public static WeekSleepChartFragment newInstance ( int totalDays ) {
         WeekSleepChartFragment fragmentFirst = new WeekSleepChartFragment();
         Bundle args = new Bundle();
@@ -144,6 +146,13 @@ public class WeekSleepChartFragment extends AbstractWeekChartFragment {
         sleepDatesText = rootView.findViewById(R.id.sleep_dates);
 
         mBalanceView = rootView.findViewById(R.id.balance);
+
+        SHOW_BALANCE = WearableApplication.getPrefs().getBoolean("charts_show_balance_sleep", true);
+        if (SHOW_BALANCE) {
+            mBalanceView.setVisibility(View.VISIBLE);
+        } else {
+            mBalanceView.setVisibility(View.GONE);
+        }
 
         if (!supportsSleepScore()) {
             sleepScoreWrapper.setVisibility(View.GONE);