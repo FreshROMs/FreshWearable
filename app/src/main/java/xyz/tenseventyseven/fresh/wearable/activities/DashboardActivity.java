f5034cd9b102f6d79ae4e677c3dbbe6a92044069
John Vincent Corcega <git@tenseventyseven.xyz>
2025-02-03 19:57:15 +0800

    Improve layout of wearable dashboard
    Changes in ToolbarLayout makes the device header too big. Adopt the example provided in the design library and use that to improve the dashboard layout.
    
    While we're at it, also improve the spacing of the dashboard contents.
    
    Signed-off-by: John Vincent Corcega <git@tenseventyseven.xyz>

--- app/src/main/java/xyz/tenseventyseven/fresh/wearable/activities/DashboardActivity.java
+++ app/src/main/java/xyz/tenseventyseven/fresh/wearable/activities/DashboardActivity.java
@@ -1,23 +1,26 @@
 package xyz.tenseventyseven.fresh.wearable.activities;
 
+import static dev.oneuiproject.oneui.widget.AdaptiveCoordinatorLayout.MARGIN_PROVIDER_ADP_DEFAULT;
+
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ListView;
 
-import androidx.activity.EdgeToEdge;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.annotation.NonNull;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.google.android.material.appbar.AppBarLayout;
 
 import java.util.List;
 import java.util.Objects;
 
-import dev.oneuiproject.oneui.layout.ToolbarLayout;
+import dev.oneuiproject.oneui.utils.DeviceLayoutUtil;
 import nodomain.freeyourgadget.gadgetbridge.devices.DeviceManager;
 import nodomain.freeyourgadget.gadgetbridge.impl.GBDevice;
 import xyz.tenseventyseven.fresh.Application;
@@ -31,8 +34,12 @@ public class DashboardActivity extends AbstractNoActionBarActivity {
 
     private WearActivityDashboardBinding binding;
     private DeviceHeader header;
-    private ToolbarLayout toolbarLayout;
     private GBDevice device;
+
+    private AppBarLayout appBar;
+
+    private AppBarListener appBarListener = new AppBarListener();
+
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -62,12 +69,13 @@ public class DashboardActivity extends AbstractNoActionBarActivity {
         super.onCreate(savedInstanceState);
 
         binding = WearActivityDashboardBinding.inflate(getLayoutInflater());
-        header = new DeviceHeader(this);
+        binding.coordinator.configureAdaptiveMargin(MARGIN_PROVIDER_ADP_DEFAULT, binding.dashboardContent);
+        header = binding.deviceHeader;
+        appBar = binding.appBar;
 
-        EdgeToEdge.enable(this);
         setContentView(binding.getRoot());
 
-        setupToolbar();
+        setupAppBar(getResources().getConfiguration());
         setupIntentListeners();
 
         // Request device info from service
@@ -80,16 +88,26 @@ public class DashboardActivity extends AbstractNoActionBarActivity {
         }
     }
 
-    private void setupToolbar() {
-        toolbarLayout = binding.toolbarLayout;
-        ViewCompat.setOnApplyWindowInsetsListener(binding.toolbarLayout, (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        setupAppBar(newConfig);
+    }
 
-        toolbarLayout.setCustomTitleView(header);
-        toolbarLayout.getAppBarLayout().seslSetCustomHeightProportion(true, 0.5f);
+    @SuppressLint("RestrictedApi")
+    private void setupAppBar(Configuration config) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            if (config.orientation != Configuration.ORIENTATION_LANDSCAPE
+                    && !isInMultiWindowMode() || DeviceLayoutUtil.INSTANCE.isTabletLayoutOrDesktop(this)) {
+                appBar.seslSetCustomHeightProportion(true, 0.4f);
+                appBar.addOnOffsetChangedListener(appBarListener);
+                appBar.setExpanded(true, false);
+            } else {
+                appBar.setExpanded(false, false);
+                appBar.seslSetCustomHeightProportion(true, 0);
+                appBar.removeOnOffsetChangedListener(appBarListener);
+            }
+        }
     }
 
     // Set up local intent listeners
@@ -108,7 +126,7 @@ public class DashboardActivity extends AbstractNoActionBarActivity {
         }
 
         header.setDevice(device);
-        toolbarLayout.setTitle(device.getAliasOrName());
+        binding.toolbar.setTitle(device.getAliasOrName());
         header.refresh();
     }
 
@@ -140,5 +158,29 @@ public class DashboardActivity extends AbstractNoActionBarActivity {
         DeviceSettingsAdapter adapter = new DeviceSettingsAdapter(this, device, deviceSettings);
         ListView listView = binding.deviceSettings;
         listView.setAdapter(adapter);
+    }
+
+    private class AppBarListener implements AppBarLayout.OnOffsetChangedListener {
+        @Override
+        public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+
+            final int totalScrollRange = appBarLayout.getTotalScrollRange();
+            final int abs = Math.abs(verticalOffset);
+
+            if (abs >= totalScrollRange / 2) {
+                header.setAlpha(0f);
+            } else if (abs == 0) {
+                header.setAlpha(1f);
+            } else {
+                float offsetAlpha = (appBarLayout.getY() / totalScrollRange);
+                float arrowAlpha = 1 - (offsetAlpha * -3);
+                if (arrowAlpha < 0) {
+                    arrowAlpha = 0;
+                } else if (arrowAlpha > 1) {
+                    arrowAlpha = 1;
+                }
+                header.setAlpha(arrowAlpha);
+            }
+        }
     }
 }
